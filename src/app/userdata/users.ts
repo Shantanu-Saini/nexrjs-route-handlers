a27f6d5304f0292880b2818dc5cb1ad2bd6f5f17
Shantanu <shantanusaini7000@gmail.com>
2024-08-10 18:36:32 +0530

    get and post method

---
+++ src/app/userdata/users.ts
@@ -0,0 +1,15 @@
+export const users = [
+    {
+        "id": 1,
+        "name": "John Doe",
+    },
+    {
+        "id": 2,
+        "name": "Jane Doe",
+    },
+    {
+        "id": 3,
+        "name": "Bob Smith",
+    }
+
+]
