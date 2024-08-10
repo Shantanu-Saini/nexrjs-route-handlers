005839c53509c4e2ebf51ea34d28aa52ed9a1ee1
Shantanu <shantanusaini7000@gmail.com>
2024-08-11 00:29:59 +0530

    query parameters

--- src/app/userdata/route.ts
+++ src/app/userdata/route.ts
@@ -1,9 +1,10 @@
+import { NextRequest } from "next/server";
 import { users } from "./users";
 
-// GET method
-export async function GET() {
-    return Response.json(users)
-}
+// // GET method
+// export async function GET() {
+//     return Response.json(users)
+// }
 
 // POST method
 export async function POST(request: Request) {
@@ -16,9 +17,19 @@ export async function POST(request: Request) {
     return new Response(
         JSON.stringify(newUser),  // or // "newuser created", // or // JSON.stringify(user),
         {
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        status: 201,
-    })
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            status: 201,
+        })
+}
+
+// query parameters
+export async function GET(request: NextRequest) {
+    const searchParams = request.nextUrl.searchParams;
+    const query = searchParams.get('query')
+    const filteredUser = query ?
+        users.filter(user => user.name.toLowerCase().includes(query)) :
+        users;
+    return Response.json(filteredUser);
 }
