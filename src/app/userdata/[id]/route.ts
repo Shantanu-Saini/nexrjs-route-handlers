c1d4977f4becb3f6bc394e66d593ec7ef3785c5b
Shantanu <shantanusaini7000@gmail.com>
2024-08-10 23:39:24 +0530

    patch request handling

--- src/app/userdata/[id]/route.ts
+++ src/app/userdata/[id]/route.ts
@@ -4,3 +4,11 @@ export async function GET(request: Request, { params }: { params: { id: string }
     const user = users.find((user) => user.id === parseInt(params.id))
     return Response.json(user);
 }
+
+export async function PATCH(request: Request, { params }: { params: { id: string } }) {
+    const body = await request.json();
+    const userIndex = users.findIndex((user) => user.id === parseInt(params.id))
+    const { name } = body;
+    users[userIndex].name = name;
+    return Response.json(users[userIndex]);
+}
