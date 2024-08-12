7920fbdeb95f093cf1c83acdaff1eed333f29abf
Shantanu <shantanusaini7000@gmail.com>
2024-08-12 18:31:55 +0530

    request and response headers

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,3 +1,24 @@
-export async function GET() {
-    return new Response("Profile Section");
+import { headers } from "next/headers";
+import { type NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+    // // request headers
+    // // way 1: to read request headers
+    // const requestHeader = new Headers(request.headers);
+    // console.log(requestHeader.get("Authorization"));
+
+    // way 2: to read request headers
+    const headerList = headers();
+    const authorizationHeader = headerList.get("Authorization");
+    console.log(authorizationHeader);
+    
+    
+    // return new Response("Profile Section")
+
+    // response headers
+    return new Response("<h1>Profile Section</h1>", {
+        headers: {
+            "Content-Type": "text/html",
+        },
+    });
 }
